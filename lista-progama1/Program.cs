314a779f09f21c0e51a9844ff05b08395c1338d4
gabrelovardex <pinto.g@aluno.ifsp.edu.br>
2023-03-08 11:21:51 -0300

    Enviando Completo

---
+++ lista-progama1/Program.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_progama1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        //    ---------------------------------------------------------------------------------------------------------------------
+
+        //    // 1 -
+
+            //int metro,milimetro;
+            //float resultado1, resultado2;
+
+            //Console.WriteLine("Digite o valor do metro: ");
+            //metro = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Digite o valor do metro: ");
+            //milimetro = int.Parse(Console.ReadLine());
+
+            //resultado1 = metro * 100;
+            //resultado2 = milimetro * 1000;
+
+            //Console.WriteLine("O valor em centimetros é: " + resultado1);
+            //Console.WriteLine("O valor em milimetro é: " + resultado2);
+
+
+            //Console.ReadKey();
+
+            //------------------------------------------------------------------------------------------------------------------------
+
+
+            // 2 -
+
+            //float f,Cfloat,Cint;
+            //int fint;
+
+            //Console.WriteLine("Digite o grau em Fahrenheit: ");
+            //f = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Digite o grau em Fahrenheit: ");
+            //fint = int.Parse(Console.ReadLine());
+
+
+            //Cfloat = (float)(f - 32.0f) * (5.0f / 9.0f);
+            //Cint = (fint - 32) * (5 / 9);
+
+
+            //Console.WriteLine("O grau em Celsius  é: " + Cfloat);
+            //Console.WriteLine("O grau em Celsius  é: " + Cint);
+
+
+            //Console.ReadKey();
+
+            //-----------------------------------------------------------------------------------------------------------------------------------
+
+            // 3 -
+
+
+            //float pe, altu, IMC;
+
+            //Console.WriteLine("Digite o valor do seu peso: ");
+            //pe = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Digite o valor da sua altura: ");
+            //altu = float.Parse(Console.ReadLine());
+
+
+            //IMC = pe / (altu * altu);
+
+            //Console.WriteLine("O seu IMC é: " + IMC);
+
+
+
+            //Console.ReadKey();
+
+            //-----------------------------------------------------------------------------------------------------------------------------------
+
+            // 4 -
+
+
+            //float result, nu1, nu2, nu3, pe1, pe2, pe3;
+
+            //Console.WriteLine("Digite um número: ");
+            //nu1 = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Digite um número: ");
+            //nu2 = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Digite um número: ");
+            //nu3 = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Digite um peso: ");
+            //pe1 = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Digite um peso: ");
+            //pe2 = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Digite um peso: ");
+            //pe3 = float.Parse(Console.ReadLine());
+
+            //result = (nu1 + nu2 + nu3 + pe1 + pe2 + pe3);
+
+
+            //Console.WriteLine("A média é: " + result / 6.0f);
+
+
+            //Console.ReadKey();
+
+            //-------------------------------------------------------------------------------------------------------------------------------
+
+            // 5 -
+
+            //float espa, temp, velomedia;
+
+            //Console.WriteLine("Digite o valor do espaço : ");
+            //espa = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Digite o valor do tempo: ");
+            //temp = float.Parse(Console.ReadLine());
+
+
+            //velomedia = espa / temp;
+
+            //Console.WriteLine("A velocidade média é: " + velomedia);
+
+
+
+            //Console.ReadKey();
+
+            //----------------------------------------------------------------------------------------------------------------------
+
+            // 6 -
+
+            //float sala, por, salanovo;
+
+            //Console.WriteLine("Digite o seu salario atual: ");
+            //sala = float.Parse(Console.ReadLine());
+
+            //por = (sala * 25) / 100;
+
+            //salanovo = sala + por;
+
+            //Console.WriteLine("0 seu salario de porcentagem é: " + salanovo);
+            //Console.ReadKey();
+
+            //-------------------------------------------------------------------------------------------------------------------------
+
+            //7 -
+
+            //float dolares, reais, results;
+
+            //Console.WriteLine("Digite uma quantia em reais: ");
+            //reais = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Digite o valor do dolar: ");
+            //dolares = float.Parse(Console.ReadLine());
+
+            //results = reais / dolares;
+
+            //Console.WriteLine("A sua quantia em dolares é: " + results);
+            //Console.ReadKey();
+
+            //----------------------------------------------------------------------------------------------------------------------------
+
+            // 8 -
+
+            //int nume, results;
+
+            //Console.WriteLine("Digite um número: ");
+            //nume = int.Parse(Console.ReadLine());
+
+            //results = (nume * 3 + 1) + (nume * 2 - 1);
+
+            //Console.WriteLine("O valor é: " + results);
+            //Console.ReadKey();
+
+            //-----------------------------------------------------------------------------------------------------------------------------
+
+            // 9 -
+
+            //int uni, dez, cent, milh, nume;
+
+            //Console.WriteLine("Digite um número com 4 casas: ");
+            //nume = int.Parse(Console.ReadLine());
+
+            //milh = nume / 1000;
+            //cent = (nume / 100) % 10;
+            //dez = (nume / 10) % 10;
+            //uni = nume % 10;
+
+            //Console.WriteLine(milh);
+            //Console.WriteLine(cent);
+            //Console.WriteLine(dez);
+            //Console.WriteLine(uni);
+
+            //Console.ReadKey();
+
+            //-------------------------------------------------------------------------------------------------------------------------------
+
+            // 10 -
+
+            //double patrimonio, pat2, invest;
+
+            //Console.WriteLine("Quanto Marquesito investiu no inicio: ");
+            //invest = double.Parse(Console.ReadLine());
+
+            //pat2 = invest - ((invest * 3.4) / 100);
+            //pat2 = (1 + 0.028);
+        }
+    }
+}
